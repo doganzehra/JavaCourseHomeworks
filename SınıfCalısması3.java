6fa421f112c9fd81ed4cf80d71d74adabe256671
Zehra Doğan <77925097+doganzehra@users.noreply.github.com>
2022-04-09 21:08:39 +0300

    Create SınıfCalısması3.java

---
+++ SınıfCalısması3.java
@@ -0,0 +1,29 @@
+/*----------------------------------------------------------------------------------------------------------------------,
+	Math sınıfının min ve max metotları ile 3 tane sayının en küçüğünün ve en büyüğünün bulunması
+----------------------------------------------------------------------------------------------------------------------*/
+package csd;
+
+class App {	
+	public static void main(String [] args)
+	{
+		FindNumbers.run();
+	}
+}
+
+public class FindNumbers {
+	
+	public static void run() 
+	{
+		java.util.Scanner kb =  new java.util.Scanner(System.in);
+		System.out.printf("Please enter the first number:");
+		int a = Integer.parseInt(kb.nextLine());
+		System.out.printf("Please enter the second number:");
+		int b = Integer.parseInt(kb.nextLine());
+		System.out.printf("Please enter the third number:");
+		int c = Integer.parseInt(kb.nextLine());
+		
+		System.out.printf("min(%d, %d, %d) = %d%n", a, b, c, Math.min(Math.min(a, b), c));
+		System.out.printf("max(%d, %d, %d) = %d%n", a, b, c, Math.max(Math.max(a, b), c));
+
+	}
+}
