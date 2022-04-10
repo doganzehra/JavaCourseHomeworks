82aa1729dfbc464cfd253c073d046d86cfcb001b
Zehra Doğan <77925097+doganzehra@users.noreply.github.com>
2022-04-10 20:24:50 +0300

    Create SınıfCalısması10.java

---
+++ SınıfCalısması10.java
@@ -0,0 +1,36 @@
+/*----------------------------------------------------------------------------------------------------------------------	 
+	 Sınıf Çalışması: Parametresi ile aldığı int türden bir sayının tersini döndüren reversed isimli metodu NumberUtil
+	 sınıfı içerisinde yazınız ve aşağıdaki kod ile test ediniz.
+	 
+	 123 -> 3 -> 3 * 10 + 2 = 32 -> 32 * 10 + 1 = 321
+----------------------------------------------------------------------------------------------------------------------*/
+package csd;
+class App {	
+	public static void main(String [] args)
+	{
+		NumberUtil.run();
+	}
+}
+
+public class NumberUtil {
+	
+	public static void run()
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		System.out.println("Please enter the number:");
+		int number = Integer.parseInt(kb.nextLine());
+		System.out.printf("%d sayısının ters hali %d'dir.", number, reversed(number));
+	}
+	
+	public static int reversed(int number)
+    {
+        int reverseNumber = 0;
+		while (number > 0) {
+            reverseNumber = reverseNumber * 10 + number % 10;
+            number /= 10;
+        }
+        return reverseNumber;
+    }
+}
+
+
