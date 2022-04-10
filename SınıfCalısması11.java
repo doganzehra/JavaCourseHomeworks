0ea6dec9e7348a39a99a509d0aa45fb5df2f22e5
Zehra Doğan <77925097+doganzehra@users.noreply.github.com>
2022-04-10 21:53:30 +0300

    Update SınıfCalısması11.java

--- SınıfCalısması11.java
+++ SınıfCalısması11.java
@@ -28,6 +28,9 @@ public class NumberUtil {
 	
 	public static int pow(int a, int b)
     {
+		if(b < 0) {
+			return 1;
+		}
         int result = 1;
         for(int i = 1; i<= b; i++) {
         	result *= a;
