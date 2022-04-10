262d3fcde418e8d02a45c09f9c18a2e68f92e09f
Zehra Doğan <77925097+doganzehra@users.noreply.github.com>
2022-04-10 21:48:51 +0300

    Create SınıfCalısması11.java

---
+++ SınıfCalısması11.java
@@ -0,0 +1,38 @@
+/*----------------------------------------------------------------------------------------------------------------------	 
+	 Sınıf Çalışması: Parametresi ile aldığı int türden a ve b değerleri için a'nın b-inci kuvvetini döndüren pow isimli
+	 metodu NumberUtil sınıfı içerisinde yazınız.
+	 Açıklamalar:
+	 - Math sınıfının pow metodu kullanılmayacaktır
+	 - Metot negatif b değerleri için 1(bir) değerine geri dönecektir
+----------------------------------------------------------------------------------------------------------------------*/
+package csd;
+
+class App {	
+	public static void main(String [] args)
+	{		
+		NumberUtil.run();		
+	}
+}
+
+public class NumberUtil {
+	
+	public static void run()
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		System.out.print("Please enter the number:");
+		int a = Integer.parseInt(kb.nextLine());
+		System.out.print("Please enter the power:");
+		int b = Integer.parseInt(kb.nextLine());
+		System.out.printf("%d sayısının %d üssü %d'dir.", a, b, pow(a, b));
+	}
+	
+	public static int pow(int a, int b)
+    {
+        int result = 1;
+        for(int i = 1; i<= b; i++) {
+        	result *= a;
+        }
+		
+        return result;
+    }
+}
