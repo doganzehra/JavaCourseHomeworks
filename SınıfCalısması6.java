008ff14f0c3ca0fe6e0ed5262269e450d650fde1
Zehra Doğan <77925097+doganzehra@users.noreply.github.com>
2022-04-10 16:16:04 +0300

    Create SınıfCalısması6.java

---
+++ SınıfCalısması6.java
@@ -0,0 +1,4 @@
+/*----------------------------------------------------------------------------------------------------------------------	 
+	 Sınıf Çalışması: Klavyeden sıfır girilene kadar alınan sayıların pozitif ve negatif olanlarının ayrı ayrı toplamını 
+	 bulan programı yazınız. Programda hiç negatif veya pozitif sayı girilmemişse uygun mesajlar verilmelidir
+----------------------------------------------------------------------------------------------------------------------*/
