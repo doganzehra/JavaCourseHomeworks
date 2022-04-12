7b9497b8360cf0e049fe612fec009e25b456efc9
Zehra Doğan <77925097+doganzehra@users.noreply.github.com>
2022-04-12 12:45:00 +0300

    Update HW3.java

--- HW3.java
+++ HW3.java
@@ -79,3 +79,73 @@ public class FindNumber {
 	
 }
 
+/*-----------------------------------------------------------------------------------------------------------------------------------------
+ 2. Aşağıda açıklanan isPrimeX isimli metodu yazınız:
+isPrimeX metodu içerisinde, gönderilen argumanın asal olup olmadığı test edilecek, eğer sayı asal ise bu kez sayının 
+basamak değerleri toplanarak elde edilen sayının asal olup olmadığı test edilecektir. Bu işlem sonuçta tek basamaklı bir sayı 
+kalana kadar devam edilecektir. Eğer en son elde edilen tek basamaklı sayı dahil tüm sayılar asal ise isPrimeX metodu true
+değerine geri dönecektir. Eğer herhangi bir kademede asal olmayan bir sayı elde edilirse metot false değerine geri 
+dönecektir. 
+ ------------------------------------------------------------------------------------------------------------------------------------------*/
+package csd;
+
+class App {	
+	public static void main(String [] args)
+	{
+		ShowPrime.run();
+	}
+}
+
+public class ShowPrime {
+
+	public static void run() 
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		System.out.print("Lütfen  bir sayı giriniz:");
+		int number = Integer.parseInt(kb.nextLine());
+		System.out.print(isPrimeX(number));
+	}
+	
+	public static boolean isPrime(int number) {
+		for(int i = 2; i < number; i++) {
+			if(number % i == 0) {
+				return false;
+			}
+		}
+    	return true;
+	}
+	
+	public static int digitSum(int number) 
+	{
+		int sum=0;
+		while(number != 0)
+		{
+			sum = number % 10;
+			number /= 10;
+		}
+		return sum;
+	}
+	
+	public static boolean isPrimeX(int number) 
+	{
+		while(number != 0) {
+			if(isPrime(number)) {
+				number = digitSum(number);
+				return true;
+			}
+			return false;
+		}
+		return false;
+	}	
+}
+/*-----------------------------------------------------------------------------------------------------------------------------------------
+3. Parametresi ile aldığı int türden bir sayının basamaksal kökünü bulan calculateDigitalRoot isimli metodu yazınız ve test 
+ediniz
+Açıklamalar:
+• Bir sayının basamaksal kökü tüm basamaklarının toplanması ile elde edilen sayıdır. Basamakları toplama işlemi,
+tek basamaklı bir sayı elde edilene kadar sürdürülür:
+36987 -> 3 + 6 + 9 + 8 + 7 = 33
+33 -> 3 + 3 = 6 → basamaksal kök
+ ------------------------------------------------------------------------------------------------------------------------------------------*/
+
+
