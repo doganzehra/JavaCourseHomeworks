0a53f4c246a361bd51347ce0654361f62e62f71e
Zehra Doğan <77925097+doganzehra@users.noreply.github.com>
2022-04-12 18:36:11 +0300

    Update SınıfCalısması16.java

--- SınıfCalısması16.java
+++ SınıfCalısması16.java
@@ -7,3 +7,46 @@
 		- Metot pozitif olmayan n değerleri için kontrol yapmayacaktır
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
+
+class App {	
+	public static void main(String [] args)
+	{
+		NumberUtil.run();
+	}
+}
+
+public class NumberUtil {
+
+	public static void run() 
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		
+		for (;;) {
+			System.out.print("Bir sayı giriniz:");
+			int n = Integer.parseInt(kb.nextLine());
+			
+			if (n < 1)
+				break;
+			
+			System.out.printf("%d. Fibonacci sayısı:%d%n", n, NumberUtil.fibonacciNumbers(n));
+		}
+		System.out.println("Tekrar yapıyor musunuz?");
+	}
+	
+	 public static int fibonacciNumbers(int number)
+	 {
+		if( number <= 2) {
+			return number - 1;
+		} 
+		
+		int x = 0, y = 1, sum = 0;
+		for(int i = 2; i < number; i++) {
+			sum = x + y;
+			x = y;
+			y = sum;
+		}
+		return sum;
+	
+	 }
+}
+
