a9d034a456bc919c466486f552bc7dec808623e1
Zehra Doğan <77925097+doganzehra@users.noreply.github.com>
2022-04-12 16:09:43 +0300

    Create SınıfCalısması16.java

---
+++ SınıfCalısması16.java
@@ -0,0 +1,9 @@
+/*----------------------------------------------------------------------------------------------------------------------	 
+	Sınıf Çalışması: Parametresi ile aldığı n sayısı için n-inci Fibonacci sayısını döndüren fibonacciNumber isimli 
+	metodu NumberUtil sınıfı içerisinde ve aşağıdaki kod ile test ediniz
+	Açıklama:
+		- Fibonnaci sayıları
+			0 1 1 2 3 5 8 13 21 34 ...
+		- Metot pozitif olmayan n değerleri için kontrol yapmayacaktır
+----------------------------------------------------------------------------------------------------------------------*/
+package csd;
